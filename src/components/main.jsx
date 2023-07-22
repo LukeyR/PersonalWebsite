a35390d5dcd5c91edc99750efc76deeca0490d5a
Luke Roberts <robertsluke3@gmail.com>
2023-07-22 20:00:17 +0100

    Now with 100% less console.logs

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -5,14 +5,13 @@ function Introduction({useDarkMode}) {
   return (
     <>
       <h1>Luke Roberts</h1>
+      <p>Front Office Software Engineer Specialising In Energy Derivatives @ Dare</p>
       <a href = "mailto: contact@ltroberts.co.uk">contact@ltroberts.co.uk</a>
-      {/*<h3>{useDarkMode ? "Dark mode on" : "Dark mode off"}</h3>*/}
       </>
   )
 }
 
 function Main({useDarkMode, setUseDarkMode, defaultDarkMode}) {
-  console.log("here")
   return (
       <>
       <DarkModeToggle setUseDarkMode={setUseDarkMode} useDarkMode={useDarkMode} className={"darkModeToggle"} defaultDarkMode={defaultDarkMode}/>
