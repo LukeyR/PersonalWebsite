c69b6116d9036bf966a962ba86791f1af83f3502
Luke Roberts <contact@ltroberts.co.uk>
2025-01-22 18:37:25 +0000

    Minors

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -106,7 +106,6 @@ function Main({useDarkMode, setUseDarkMode, defaultDarkMode}) {
                         />
                     </Grid>
                     <Grid
-                        item
                         xs={12}
                         md={1}
                     >
